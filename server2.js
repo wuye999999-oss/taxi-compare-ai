fb639a6350268e7298b60f64f246a8c2196705bf
wuye999999-oss <wuye999999@gmail.com>
2026-05-19 19:17:26 +0800

    chore: remove legacy server2.js

--- server2.js
+++
@@ -1,6 +0,0 @@
-export * from './server-clean.js';
-
-if (process.argv[1] && import.meta.url === new URL(`file://${process.argv[1]}`).href) {
-  const { startServer } = await import('./server-clean.js');
-  startServer();
-}
